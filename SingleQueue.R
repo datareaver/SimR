fbb0ede7e7a809cb0635af3f61ffaeb4d9503a6a
Thomas Roh <thmsroh@gmail.com>
2015-07-02 14:28:52 -0500

    WIP cleaning
    removed old function. working simmer implemenation. Need to figure out
    more efficient way

--- SingleQueue.R
+++ SingleQueue.R
@@ -7,9 +7,11 @@ library(simmer)
 set.seed(103)
 rm(list=ls())
 
+u <- paste0("rexp(1,",server_rate,")")
+
 path <- create_trajectory("Path") %>%
   add_seize_event("server",1.0) %>%
-  add_timeout_event("rexp(1,3)") %>%
+  add_timeout_event(u) %>%
   add_release_event("server",1.0)
 
 arrivals <- list(Hour=1:168,Load = runif(168,min=3,max=6))
@@ -37,11 +39,9 @@ loop_simulations <- function(Hour,Load) {
 
 results <- mapply(loop_simulations,arrivals$Hour,arrivals$Load,SIMPLIFY = FALSE) %>% rbind_all()
 
-sdresults %>% group_by(Hour) %>% summarize(mean(waiting_time))
+results %>% group_by(Hour) %>% summarize(Mean = mean(waiting_time))
 
-mean(results$waiting_time)+sd(results$waiting_time)*1.96/sqrt(length(results$waiting_time))
-
-group_by(
+mean(results$waiting_time)+(-1:1)*sd(results$waiting_time)*qt(.975,length(results$waiting_time))/sqrt(length(results$waiting_time))
 
 # View(results %>% group_by(Hour,replication) %>% summarize(mean(waiting_time)))
 # View(results %>% mutate(start_time+end_time,flow_time-activity_time))
@@ -57,34 +57,29 @@ group_by(
 #   data.frame(Replication=entity_per_rep,Interarrival=rexp(length(entity_per_rep),(1/poisson_parameter))) %>%
 #     group_by(Replication) %>%
 #     mutate(Arrival_Time=cumsum(Interarrival)) %>%
-#     filter(Arrival_Time < period) %>%
-#     ungroup()
+#     filter(Arrival_Time < period)
 # }
 
-arrivals <- create_poisson_arrivals(poisson_parameter=10,reps=10,period=60)
+create_poisson_arrivals <- function(poisson_parameter=1,period=60) {
+  Entity <- 0:rpois(1,poisson_parameter)
+  data.frame(Entity,Interarrival=rexp(length(Entity),1/poisson_parameter)) %>%
+    mutate(Interval_Arrival_Time=cumsum(Interarrival)) %>%
+    filter(Interval_Arrival_Time < period,Entity > 0)
+}
 
-for (i in
+create_poisson_arrivals(1)
+create_poisson_arrivals(load)
 
-# for (arrival in arrivals) {
-#
-#   rm(sim)
-#   sim <- create_simulator(sim_name = "Single Queue",n=1000,until = 60) %>%
-#     add_resource(name="server",1)
-#
-#   rep <- 1
-#   poisson_rate <- arrival
-#
-#   for (n in rpois(sim@n,poisson_rate)) {
-#     arrival_time <- rexp(1,1/poisson_rate)
-#     for (i in 1:(n+1)) {
-#       add_entity(sim,path,name=paste0("Customer_",i,rep),activation_time = arrival_time)
-#       arrival_time <- arrival_time + rexp(1,1/poisson_rate)
-#     }
-#     rep <- rep+1
-#   }
-#   simmer(sim)
-#
-#   results <- data.frame(get_entity_monitor_values(sim, aggregated = TRUE),rep)
-#   results_df <- rbind
-#
-# }
+load <- read.table('C:/Users/m097845/Google Drive/Scripts/Staff.Scheduling.Tool/load.csv',sep=",",
+                   header=FALSE,row.names=paste0('t',0:167),col.names="Value") %>%
+  apply(2,as.list)
+
+
+arrivals <- lapply(load[['Value']],create_poisson_arrivals) %>%
+  do.call(what="rbind")
+
+Time_ID <- as.numeric(gsub("t","",sapply(strsplit(row.names(arrivals),"\\."),getElement,1)))*60
+
+arrivals <- data.frame(Time_ID,arrivals) %>%
+  mutate(Arrival_Time = Time_ID+Interval_Arrival_Time)
+
